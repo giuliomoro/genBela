3fd05d027677a63daa238f6097348f27508ef0f0
Giulio Moro <giuliomoro@yahoo.it>
2018-06-30 15:41:52 +0100

    Using native non-interleaved buffers and resampling, giving gen_exported::perform direct access to Bela's audio and analog buffers

--- render.cpp
+++ render.cpp
@@ -2,14 +2,27 @@
 #include "gen_exported.h"
 
 CommonState* gState = NULL;
-t_sample* gInputs = NULL;
-t_sample* gOutputs = NULL;
+
+void Bela_userSettings(BelaInitSettings *settings)
+{
+	settings->uniformSampleRate = 1;
+	settings->interleave = 0;
+	// settings->analogOutputsPersist = 0; // only disable persistence if you are handling all the analog outs as signals
+}
 
 bool setup(BelaContext *context, void *userData)
 {
+	if(!(context->flags & BELA_FLAG_INTERLEAVED))
+	{
+		fprintf(stderr, "You need a non-interleaved buffer\n");
+		return 0;
+	}
+	if(context->analogSampleRate != context->audioSampleRate)
+	{
+		fprintf(stderr, "You need the analog and audio channels to have the same sampling rate\n");
+		return 0;
+	}
 	gState = (CommonState*)gen_exported::create(context->audioSampleRate, context->audioFrames);
-	gInputs = new t_sample[context->audioFrames * context->audioInChannels];
-	gOutputs = new t_sample[context->audioFrames * context->audioOutChannels];
 	return true;
 }
 
@@ -17,39 +30,32 @@ void render(BelaContext *context, void *userData)
 {
 	int nFrames = context->audioFrames;
 
-	int nInChannels = context->audioInChannels;
+	int nInChannels = context->audioInChannels + context->analogInChannels;
+	// taking pointers to the input channels
 	t_sample* ip[nInChannels];
-	for (int i = 0; i < nInChannels; i++) {
-		for (int j = 0; j < nFrames; j++) {
-			*(gInputs + ((i * nFrames) + j)) = audioRead(context, j, i);
-		}
-		ip[i] = gInputs + (i * nFrames);
+	for (unsigned int i = 0; i < context->audioInChannels; i++) {
+		ip[i] = (t_sample*)context->audioIn + (i * nFrames);
+	}
+	for (unsigned int i = 0; i < context->audioInChannels; i++) {
+		ip[i + context->audioInChannels] = (t_sample*)context->analogIn + (i * nFrames);
 	}
 
-	int nOutChannels = context->audioOutChannels;
+	int nOutChannels = context->audioOutChannels + context->analogOutChannels;
+	// taking pointers to the output channels
 	t_sample* op[nOutChannels];
-	for (int i = 0; i < nOutChannels; i++) {
-		op[i] = gOutputs + (i * nFrames);
+	for (unsigned int i = 0; i < context->audioOutChannels; i++) {
+		op[i] = (t_sample*)context->audioOut + (i * nFrames);
+	}
+	for (unsigned int i = 0; i < context->audioOutChannels; i++) {
+		op[i + context->audioOutChannels] = (t_sample*)context->analogOut + (i * nFrames);
 	}
 
 	gen_exported::perform(gState, ip, nInChannels, op, nOutChannels, nFrames);
-
-	for (int i = 0; i < nOutChannels; i++) {
-		for (int j = 0; j < nFrames; j++) {
-			audioWrite(context, j, i, *(gOutputs + ((i * nFrames) + j)));
-		}
-	}
 }
 
 void cleanup(BelaContext *context, void *userData)
 {
-    if (gState) {
-        gen_exported::destroy(gState);
-    }
-		if (gInputs) {
-			delete[] gInputs;
-		}
-		if (gOutputs) {
-			delete[] gOutputs;
-		}
+	if (gState) {
+		gen_exported::destroy(gState);
+	}
 }
